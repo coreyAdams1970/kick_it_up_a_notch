7e772cc70e53761b97f6ca1f86e6b1fcccc08340
corey smaller <coreysmaller@gmail.com>
2021-03-12 11:13:15 -0700

    add google analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Button from "../components/button"
+import React, { useEffect } from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-GQ7PW6FH0Z');
 
 export default function IndexPage(props) {
 
-  const siteTitle = "Kick It Up A Notch Alpine and Desert Design"
+  const siteTitle = "Kick It Up A Notch Alpine and Desert Design";
+
+  useEffect(() => {
+    if (typeof "window" !== "undefined") {
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+  }, [])
 
   return (
     <Layout location={props.location} title={siteTitle}>
@@ -15,8 +20,8 @@ export default function IndexPage(props) {
         title="Home"
         keywords={[`desert landscaping`, `landscaping`, `landscape design`, `arizona landscape design`]}
       />
-      <img style={{ margin: 0, width:100, height:100 }} src="./images/liz.jpeg" alt="liz martin" />
-      
+      <img style={{ margin: 0, width: 100, height: 100 }} src="./images/liz.jpeg" alt="liz martin" />
+
       <h3 className="text-white">Welcome to Kick It Up A Notch.</h3>
     </Layout>
   )
